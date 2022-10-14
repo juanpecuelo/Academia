72f7aebe813def05690bba9cb9f6362d554accb0
juanpecuelo <juanpecuelo@gmail.com>
2022-10-14 14:33:12 +0200

    Academia dev 1.4
    - Se ha añadido el theme del cardview
    
    -Se ha puesto un ripple effect al cardview con color, y bordes
    
    - Se sigue avanzando con el modo oscuro

---
+++ app/src/main/java/com/juanpecuelo/academia/DoubleViewPager.java
@@ -0,0 +1,14 @@
+package com.juanpecuelo.academia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DoubleViewPager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_double_view_pager);
+    }
+}
